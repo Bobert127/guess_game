d24e74f68b539cc6fd909b587fe9e0df2637da3c
Robert <roberttrzaska@outlook.com>
2021-05-15 13:08:25 +0200

    nowa funkcja

---
+++ app.py
@@ -0,0 +1,12 @@
+def get_user_range():
+    user_digit_min = input("Provide digit for min range:\n")
+    user_digit_max = input("Provide digit for max range:\n")
+
+    return user_digit_min, user_digit_max
+
+print(get_user_range())
+
+ #   print(user_digit_min)
+ #   print(user_digit_max)
+
+
